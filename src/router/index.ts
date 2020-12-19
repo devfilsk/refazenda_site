c499a3fa4a3bbe386ba7db84ece9e2d5675d9f13
Filipe Maciel <devfilsk@gmail.com>
2020-12-18 23:43:59 -0300

    Routers rules

--- src/router/index.ts
+++ src/router/index.ts
@@ -105,8 +105,7 @@ router.beforeEach((to, from, next) => {
   const token = localStorage.getItem(TOKEN_LABEL);
   if (to.name == 'Login' && token) {
     next({ path: '/dashboard' })
-  }
-  if (to.matched.some(record => record.meta.login)) {
+  } else if (to.matched.some(record => record.meta.login)) {
     if (!localStorage.getItem(TOKEN_LABEL)) {
       next("/login");
     } else {
